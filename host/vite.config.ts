041f72d8279b6b08645755c98849e971098b134b
William Franklin <wilfrank84@gmail.com>
2023-11-30 16:37:23 -0300

    feat: test env

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig, loadEnv } from "vite";
 
 const env = loadEnv("all", process.cwd());
 
+console.log("env ===>", env);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
