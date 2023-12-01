7e4fdde730beeb5500dfccf54daaffc6bc02f669
William Franklin <wilfrank84@gmail.com>
2023-12-01 10:32:06 -0300

    feat: add mfe shared

---
+++ mfe-shared/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-3xl font-bold underline">MFE Shared</h1>;
+}
+
+export default App;
---
+++ mfe-shared/vite.config.ts
@@ -0,0 +1,24 @@
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "shared_app",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./store": "./src/store",
+      },
+      shared: ["react", "react-dom", "zustand"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
